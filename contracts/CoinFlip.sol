257ca5ff81bc0f86ae421b6e9b3eecffe1869853
Tom <demart.tom@gmail.com>
2022-03-20 16:46:57 -0700

    Adding initial smart contract which compiles without error. Still need to test and deploy and will bugfix or add features down the line

---
+++ contracts/CoinFlip.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.8.0;
+
+contract CoinFlip {
+   // return
+   //    0 = heads, 1 = tails
+   function _flip() private view returns (bool) {
+      return uint(keccak256(abi.encodePacked(block.timestamp))) % 2 == 1;
+   }
+
+   // arguments
+   //    guess: 0 = head, 1 = tails
+   // return
+   //    win: 0 = lose, 1 = win
+   function flip(bool guess) public view returns (bool) {
+      return guess == _flip();
+   }
+}
