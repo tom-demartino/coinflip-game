5aa637c0c87b77aafb0abe6c090b68922b38ce45
Tom <demart.tom@gmail.com>
2022-03-24 21:19:05 -0700

    Added event which will likely be used by JS to read result of transaction and also increments block count for dev blockchains which will be useful for testing

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -2,6 +2,9 @@
 pragma solidity >=0.8.0;
 
 contract CoinFlip {
+   // Event posted to chain that details result of coinflip
+   event Result(address indexed _from, bool _guess, bool _win);
+
    // return
    //    0 = heads, 1 = tails
    function _flip() private view returns (bool) {
@@ -12,7 +15,9 @@ contract CoinFlip {
    //    guess: 0 = head, 1 = tails
    // return
    //    win: 0 = lose, 1 = win
-   function flip(bool guess) public view returns (bool) {
-      return guess == _flip();
+   function flip(bool _guess) public returns (bool) {
+      bool win = _guess == _flip();
+      emit Result(msg.sender, _guess, win);
+      return win;
    }
 }
