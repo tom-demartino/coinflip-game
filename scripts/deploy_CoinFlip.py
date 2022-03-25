072fc6a2c17a4c0ce10e86279973fca2e5f445f6
Tom <demart.tom@gmail.com>
2022-03-24 21:20:16 -0700

    Fixed deploy script. Tested and appears to work correctly now

--- scripts/deploy_CoinFlip.py
+++ scripts/deploy_CoinFlip.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from brownie import accounts
+from brownie import CoinFlip, accounts
 
-def deploy_CoinFlip(acct_idx=None):
-   account = accounts(acct_idx)
-   account.deploy(CoinFlip)
+def deploy_CoinFlip(acct_idx=0):
+   account = accounts[acct_idx]
+   contract = CoinFlip.deploy({"from": account})
+   return contract
 
-def main(acct_idx=None):
+def main(acct_idx=0):
    deploy_CoinFlip(acct_idx)
